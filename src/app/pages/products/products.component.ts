4e9ee05e1e744e2a46725b58198c02a34f97a425
Abhishek Gupta <abhishekedu4979@gmail.com>
2022-11-11 18:50:50 +0530

    nav style update

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -78,20 +78,6 @@ items= [{
   product_rating : 4,
   product_rating_count: 555,
   product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
-},{
-  product_name: 'Lakme',
-  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
-  product_benefits: 'Glows skin',
-  product_rating : 3,
-  product_rating_count: 555,
-  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
-},{
-  product_name: 'Lakme',
-  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
-  product_benefits: 'Glows skin',
-  product_rating : 5,
-  product_rating_count: 555,
-  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
 }];
   constructor() { }
 
