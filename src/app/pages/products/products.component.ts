33859d499f45d4231c5ba0ff50ea5f2fe9326d7e
Abhishek Gupta <abhishekedu4979@gmail.com>
2022-11-11 16:05:00 +0530

    product page ready

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -6,10 +6,99 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-
+showLabel= false;
+items= [{
+  product_name: 'Lakme',
+  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
+  product_benefits: 'Glows skin',
+  product_rating : 4,
+  product_rating_count: 555,
+  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
+},{
+  product_name: 'Lakme',
+  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
+  product_benefits: 'Glows skin',
+  product_rating : 3,
+  product_rating_count: 555,
+  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
+},{
+  product_name: 'Lakme',
+  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
+  product_benefits: 'Glows skin',
+  product_rating : 5,
+  product_rating_count: 555,
+  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
+},{
+  product_name: 'Lakme',
+  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
+  product_benefits: 'Glows skin',
+  product_rating : 4,
+  product_rating_count: 555,
+  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
+},{
+  product_name: 'Lakme',
+  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
+  product_benefits: 'Glows skin',
+  product_rating : 3,
+  product_rating_count: 555,
+  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
+},{
+  product_name: 'Lakme',
+  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
+  product_benefits: 'Glows skin',
+  product_rating : 5,
+  product_rating_count: 555,
+  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
+},{
+  product_name: 'Lakme',
+  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
+  product_benefits: 'Glows skin',
+  product_rating : 4,
+  product_rating_count: 555,
+  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
+},{
+  product_name: 'Lakme',
+  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
+  product_benefits: 'Glows skin',
+  product_rating : 3,
+  product_rating_count: 555,
+  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
+},{
+  product_name: 'Lakme',
+  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
+  product_benefits: 'Glows skin',
+  product_rating : 5,
+  product_rating_count: 555,
+  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
+},
+{
+  product_name: 'Lakme',
+  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
+  product_benefits: 'Glows skin',
+  product_rating : 4,
+  product_rating_count: 555,
+  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
+},{
+  product_name: 'Lakme',
+  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
+  product_benefits: 'Glows skin',
+  product_rating : 3,
+  product_rating_count: 555,
+  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
+},{
+  product_name: 'Lakme',
+  product_url: 'https://cdn.shopify.com/s/files/1/0014/3514/0183/products/23414_H_8901030907487_600x.jpg',
+  product_benefits: 'Glows skin',
+  product_rating : 5,
+  product_rating_count: 555,
+  product_redirect_url: 'https://www.lakmeindia.com/collections/serums/products/23414-lapr-brighten-srm-w-vita-resorcinol-30m',
+}];
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
+ 
+
