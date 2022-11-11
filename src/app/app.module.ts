00fe1e4d893ea33b0b81d8d06a0265a63f8cc3ca
Jeetendras singh <jeetendra.singh@example.com>
2022-11-11 09:15:45 +0530

    added landing page and image-capure component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScanComponent } from './pages/scan/scan.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { ImageCaptureComponent } from './common/image-capture/image-capture.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ScanComponent,
-    ProductsComponent
+    ProductsComponent,
+    ImageCaptureComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/common/image-capture/image-capture.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-capture',
+  templateUrl: './image-capture.component.html',
+  styleUrls: ['./image-capture.component.scss']
+})
+export class ImageCaptureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
