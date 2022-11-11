dbdbf3b6f7bdbcec5d41a815b818e053ac3942e6
Abhishek Gupta <abhishekedu4979@gmail.com>
2022-11-11 17:59:37 +0530

    food-diet page added

--- src/app/pages/food-diet/food-diet.component.ts
+++ src/app/pages/food-diet/food-diet.component.ts
@@ -6,7 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./food-diet.component.scss']
 })
 export class FoodDietComponent implements OnInit {
-
+  showLabel= false;
+  items= [{
+    product_name: 'Sprouts',
+    product_url: 'https://raw.githubusercontent.com/Abhishek0402/Recipe_App/master/src/sprouts.jpg',
+    product_benefits: 'Vitamin C present in sprouts promotes a clear skinthrough the production of collagen.',
+  },{
+    product_name: 'Sprouts',
+    product_url: 'https://raw.githubusercontent.com/Abhishek0402/Recipe_App/master/src/sprouts.jpg',
+    product_benefits: 'Vitamin C present in sprouts promotes a clear skinthrough the production of collagen.',
+  },{
+    product_name: 'Sprouts',
+    product_url: 'https://raw.githubusercontent.com/Abhishek0402/Recipe_App/master/src/sprouts.jpg',
+    product_benefits: 'Vitamin C present in sprouts promotes a clear skinthrough the production of collagen.',
+  },{
+    product_name: 'Sprouts',
+    product_url: 'https://raw.githubusercontent.com/Abhishek0402/Recipe_App/master/src/sprouts.jpg',
+    product_benefits: 'Vitamin C present in sprouts promotes a clear skinthrough the production of collagen.',
+  },{
+    product_name: 'Sprouts',
+    product_url: 'https://raw.githubusercontent.com/Abhishek0402/Recipe_App/master/src/sprouts.jpg',
+    product_benefits: 'Vitamin C present in sprouts promotes a clear skinthrough the production of collagen.',
+  },{
+    product_name: 'Sprouts',
+    product_url: 'https://raw.githubusercontent.com/Abhishek0402/Recipe_App/master/src/sprouts.jpg',
+    product_benefits: 'Vitamin C present in sprouts promotes a clear skinthrough the production of collagen.',
+  },{
+    product_name: 'Sprouts',
+    product_url: 'https://raw.githubusercontent.com/Abhishek0402/Recipe_App/master/src/sprouts.jpg',
+    product_benefits: 'Vitamin C present in sprouts promotes a clear skinthrough the production of collagen.',
+  },{
+    product_name: 'Sprouts',
+    product_url: 'https://raw.githubusercontent.com/Abhishek0402/Recipe_App/master/src/sprouts.jpg',
+    product_benefits: 'Vitamin C present in sprouts promotes a clear skinthrough the production of collagen.',
+  },{
+    product_name: 'Sprouts',
+    product_url: 'https://raw.githubusercontent.com/Abhishek0402/Recipe_App/master/src/sprouts.jpg',
+    product_benefits: 'Vitamin C present in sprouts promotes a clear skinthrough the production of collagen.',
+  },
+  {
+    product_name: 'Sprouts',
+    product_url: 'https://raw.githubusercontent.com/Abhishek0402/Recipe_App/master/src/sprouts.jpg',
+    product_benefits: 'Vitamin C present in sprouts promotes a clear skinthrough the production of collagen.',
+  }];
   constructor() { }
 
   ngOnInit(): void {
